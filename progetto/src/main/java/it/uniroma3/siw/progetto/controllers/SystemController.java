fc5e75b608cbdaa23e6c29d16932de4d89752ba0
berenice02 <foderaro.elisa@gmail.com>
2019-06-16 19:10:58 +0200

    aggiunta la barra di ricerca

--- progetto/src/main/java/it/uniroma3/siw/progetto/controllers/SystemController.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/controllers/SystemController.java
@@ -3,7 +3,12 @@ package it.uniroma3.siw.progetto.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import it.uniroma3.siw.progetto.services.FotografoServices;
 
@@ -17,6 +22,12 @@ public class SystemController {
 	public String root(Model model) {
 		model.addAttribute("fotografi", this.fotografo.primi10Fotografi());
 		return "home";
+	}
+	
+	@GetMapping(value = "/cerca")
+	public String cerca(@ModelAttribute("nome") String nome, Model model) {
+		model.addAttribute("fotografo", fotografo.fotografoPerNome(nome));
+		return "fotografo";
 	}
 
 }
