ebc252ef13df7908f5e0f6996159f51ad529195f
berenice02 <foderaro.elisa@gmail.com>
2019-06-17 00:59:10 +0200

    implementato il form "cerca"
    per ora funziona solo con parole singole

--- progetto/src/main/java/it/uniroma3/siw/progetto/controllers/SystemController.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/controllers/SystemController.java
@@ -1,15 +1,16 @@
 package it.uniroma3.siw.progetto.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import it.uniroma3.siw.progetto.models.Fotografo;
 import it.uniroma3.siw.progetto.services.FotografoServices;
 
 @Controller
@@ -25,9 +26,15 @@ public class SystemController {
 	}
 	
 	@GetMapping(value = "/cerca")
-	public String cerca(@ModelAttribute("nome") String nome, Model model) {
-		model.addAttribute("fotografo", fotografo.fotografoPerNome(nome));
-		return "fotografo";
+	// "String ... param" è per indicare che potrebbe essere uno o più param
+	public String cerca(Model model, @RequestParam("param") String ... param) {
+		List<Fotografo> risultato = new ArrayList<>();
+		for(String p : param) {
+			risultato.addAll(fotografo.fotografoPerNome(p));
+			risultato.addAll(fotografo.fotografoPerCognome(p));
+		}
+		model.addAttribute("fotografi", risultato);
+		return "fotografi";
 	}
 
 }
