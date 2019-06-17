3092dd441cf5549a06836ab15061cdb79eb6318b
berenice02 <foderaro.elisa@gmail.com>
2019-06-17 12:40:35 +0200

    piccole modifiche per implementazioni future

--- progetto/src/main/java/it/uniroma3/siw/progetto/models/Album.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/models/Album.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Album {
+public class Album implements Comparable<Album> {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -75,6 +75,35 @@ public class Album {
 
 	public void setFoto(List<Foto> foto) {
 		this.foto = foto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((foto == null) ? 0 : foto.hashCode());
+		result = prime * result + ((fotografo == null) ? 0 : fotografo.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Album other = (Album) obj;
+		return this.getId().equals(other.getId());
+	}
+	
+	@Override
+	public int compareTo(Album o) {
+		return (int) (o.getId()-this.getId());
 	}
 	
 }
--- progetto/src/main/java/it/uniroma3/siw/progetto/models/Foto.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/models/Foto.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Foto {
+public class Foto implements Comparable<Foto> {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -63,5 +63,36 @@ public class Foto {
 	public void setAlbum(Album album) {
 		this.album = album;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((album == null) ? 0 : album.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((fotografo == null) ? 0 : fotografo.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Foto other = (Foto) obj;
+		return this.getId().equals(other.getId());
+	}
+
+	@Override
+	public int compareTo(Foto o) {
+		return (int) (o.getId()-this.getId());
+	}
+	
+	
 
 }
