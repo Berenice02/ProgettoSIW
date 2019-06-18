d9736dbed00155e4e990bfc6786cfbad2de183ec
ilplaymaker <matteo.giunta97@gmail.com>
2019-06-19 01:47:34 +0200

    aggiunta dependency Spring Security, creato model Funzionario.java

---
+++ progetto/src/main/java/it/uniroma3/siw/progetto/models/Funzionario.java
@@ -0,0 +1,74 @@
+package it.uniroma3.siw.progetto.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Funzionario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String cognome;
+	private String codiceIdentificativo;
+	private String password;
+	private String role;
+	
+	public Funzionario(String nome, String cognome, String codiceIdentificativo, String password) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceIdentificativo = codiceIdentificativo;
+		this.password = password;
+		this.role = "ADMIN";
+	}
+	
+	public Funzionario() {
+		
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return this.cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceIdentificativo() {
+		return this.codiceIdentificativo;
+	}
+
+	public void setCodiceIdentificativo(String codiceIdentificativo) {
+		this.codiceIdentificativo = codiceIdentificativo;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}	
+}
